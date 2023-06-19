a4d07f148f37422efb5c37805601302ae70e4895
Rizky Syahri Ramadhan <ramadhancai@gmail.com>
2023-06-19 17:45:30 +0700

    feat: auth update

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,5 +1,5 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
-import { env } from "~/env.mjs";
+// import { env } from "~/env.mjs";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
@@ -7,12 +7,12 @@ import { createTRPCContext } from "~/server/api/trpc";
 export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-          );
-        }
-      : undefined,
+  // onError:
+  //   env.NODE_ENV === "development"
+  //     ? ({ path, error }) => {
+  //         console.error(
+  //           `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+  //         );
+  //       }
+  //     : undefined,
 });
