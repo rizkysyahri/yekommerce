bfee12fa2721acc8a13f1d1f02d7e8a2d5204195
Rizky Syahri Ramadhan <ramadhancai@gmail.com>
2023-06-19 18:23:00 +0700

    feat: db new update

--- src/server/db.ts
+++ src/server/db.ts
@@ -6,7 +6,7 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
